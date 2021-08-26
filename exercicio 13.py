ecd3e534ead6983be98225ce9c75362ce24fdfb9
Yasmin Michels <yamichels@gmail.com>
2021-08-26 17:25:21 -0300

    ex 13

---
+++ exercicio 13.py
@@ -0,0 +1,5 @@
+largura = int(input("Digite a largura da parede: "))
+altura = int(input("Digite a altura da parede: "))
+area = (largura*altura)
+metros = (area/2)
+print("A precisará de: ",metros, "litros de tinta")
